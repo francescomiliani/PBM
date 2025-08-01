58128b3ecc534c3c83d18fcd379642475218c4c2
francesco.miliani <75777914+francescomiliani@users.noreply.github.com>
2025-08-01 15:07:19 +0200

    Update IPBMRC2.sol
    Add import "./IPBMRC1.sol"; because is missing

--- assets/contracts/IPBMRC2.sol
+++ assets/contracts/IPBMRC2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-
+import "./IPBMRC1.sol";
 /**
  *  @dev This interface extends IPBMRC1, adding functions for working with non-preloaded PBMs.
  *  Non-preloaded PBMs are minted as empty containers without any underlying tokens of value,
